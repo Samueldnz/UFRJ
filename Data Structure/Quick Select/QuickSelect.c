d71872a10c6530344e988cc497c05e3036533db8
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2023-04-18 08:19:03 -0300

    Comments Update 1

--- Data Structure/Quick Select/QuickSelect.c
+++ Data Structure/Quick Select/QuickSelect.c
@@ -8,25 +8,32 @@ int* DynamicVectorBuilder(char* pfileName, int* pTam);
 
 int main(int argc, char *argv[])
 {
-    /*OK. Everything is working according to the plan. Enjoy! To call the program, just
-    use the following command: ./QuickSelect.exe K(a number of your desire) teste.txt*/
+    /*OK. Everything is working according to the plan. Enjoy! In order to call the program, 
+    just use the following command: ./QuickSelect.exe K(a number of your desire) teste.txt*/
 
-   
-    int int_tam, intResult, i;
+    /*The followings ones are variable, if they have the underscore, means they`re a 
+    temporary variable*/
+    int int_tam, intResult, i;  
     int int_K = atoi(argv[1]);
 
-    int* V = DynamicVectorBuilder(argv[2], &int_tam);
+    /*It builds the dynamic vector from a file*/
+    int* V = DynamicVectorBuilder(argv[2], &int_tam); 
+
 
     intResult = QuickSelect(V, 0, int_tam + 1, int_K);
     
+    /*Its prints out the result*/
     printf("%d", intResult);
     printf("\n\n");
 
+    /*Its prints out all the vector, totally desnecessary, so I`ll comment this part
+    pretty soon*/
     for(i = 0; i < int_tam; i++)
     {
         printf("%4d ", V[i]);   
     }
 
+    /*Realease the vector*/
     free(V);
 
     return 0;
@@ -99,21 +106,28 @@ int Partition(int *V, int intBegin, int intEnd)
 
 int* DynamicVectorBuilder(char* pfileName, int* pTam)
 {
+    /*Open the file and verify if they were truly open without any problems*/
     FILE* file = fopen(pfileName, "r");
-
     if(file == NULL) exit(1);
+
 
     int intFileNumber, int_n = 0, i;
 
+    /*It reads all the number inside the file ir order to know how many are in there consequently
+    the vector size*/
     while(fscanf(file, "%d", &intFileNumber) == 1)
     {
         int_n++;
     }
 
+
     int *V = (int*) malloc(int_n * sizeof(int));
     if(V == NULL) exit(1);
 
-    rewind(file);
+    rewind(file); /*Is used to read the file twice: first, to count how many numbers are in 
+    the file, and second, to store the numbers in the dynamic vector. As the first read loops 
+    through the file from start to finish, the rewind() function is used to reposition the 
+    file position indicator back to the beginning before the second read.*/
 
     for(i = 0; i < int_n; i++){
         fscanf(file, "%d", &V[i]);
