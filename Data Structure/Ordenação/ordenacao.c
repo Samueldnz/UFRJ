f9a49bb27d296033718691d1e28735ba67b35cf8
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2023-05-03 09:08:46 -0300

    Restart 1.1: Debugging input -> clean code

--- Data Structure/Ordenação/ordenacao.c
+++ Data Structure/Ordenação/ordenacao.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Constants */
 #define TRUE 1
 #define MAX_LENGTH 20
 #define ZERO 0
 
-/* Structures */
 typedef struct VERTEXNODE {
     int value;
     struct VERTEXNODE* next;
 } vertexnode;
 
-/* Function prototypes */
 void add_to_list(int value, vertexnode** head);
-void topologicalsorting(int startvertex, 
-                        vertexnode** vertextargets, 
-                        int** degreeOfvertexes, 
-                        int** visitedvertex,
-                        int** topologicalsort,
-                        int* index);
 
-/* Main function */
+
 int main() {
-     /* Variable declarations */
+
     char inputOfvertex[MAX_LENGTH];   /* Input string for the number of vertices */
-    int numberOfvertex;               /* Number of vertices */
+    int numberOfvertex;               
     char vertexAsString[MAX_LENGTH];  /* Input string for each vertex */
-    char ch;                          /* Temporary character */
+    char ch;                          
     int j, i, destination;            /* Loop variables and destination vertex */
     int *degreeOfvertexes;            /* Array to store the degree of each vertex */
     vertexnode *vertex_list;          /* Pointer to the adjacency list of each vertex */
@@ -38,20 +29,15 @@ int main() {
     numberOfvertex = atoi(inputOfvertex);
 
     vertextargets = (vertexnode**) malloc((numberOfvertex+1) * sizeof(vertexnode*));
-    if(vertextargets == NULL) {
-        exit(TRUE); 
-    }
-    for(i = 1; i <= numberOfvertex; i++){
-        vertextargets[i] = NULL;
-    }
+    if(vertextargets == NULL) exit(1); 
+
+    for(i = 1; i <= numberOfvertex; i++){vertextargets[i] = NULL;}
 
     degreeOfvertexes = (int*) malloc((numberOfvertex+1) * sizeof(int));
-    if(degreeOfvertexes == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    for(i = 1; i <= numberOfvertex; i++){
-        degreeOfvertexes[i] = 0;
-    }
+    if(degreeOfvertexes == NULL) exit(1);
+    
+    for(i = 1; i <= numberOfvertex; i++){degreeOfvertexes[i] = 0;}
+
 
     for(i = 1; i <= numberOfvertex; i++){
         vertex_list = NULL; 
@@ -79,20 +65,7 @@ int main() {
         vertextargets[i] = vertex_list;
     }
 
-    // TEST BEGINS
-    // printf("It's here, now we are gonna print out \n");
-
-    // for(i = 1; i <= numberOfvertex; i++){
-    //     printf("i: %d\n", i);
-    //     printf("%d\n", vertextargets[i]->value);
-    // }
-
-    // for(i = 0; i <= numberOfvertex; i++){
-    //     printf("Degree of index %d = %d\n", i, degreeOfvertexes[i]);
-    // } 
-    //THE END
-    
-    
+    printf("You are here! Congrats, It`s working!\n");
 
     return 0;
 }
@@ -107,13 +80,11 @@ int main() {
     Returns: nothing
 */
 void add_to_list(int value, vertexnode** head) {
-    /*Allocates memory for the new node*/
+    
     vertexnode* new_node = (vertexnode*) malloc(sizeof(vertexnode));
-    /*Sets the value of the new node to the input value*/
+    
     new_node->value = value;
-    /*Sets the next pointer of the new node to the current head*/
     new_node->next = (*head);
-    /*Updates the head pointer to point to the new node*/
     (*head) = new_node;
 }
 
