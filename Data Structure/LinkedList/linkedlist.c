743906c0f8d2a939f0c1db03fb4e8fb36e2c1df0
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2023-05-21 13:09:44 -0300

    Search and Delete Function

--- Data Structure/LinkedList/linkedlist.c
+++ Data Structure/LinkedList/linkedlist.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 typedef struct _NODE
 {
@@ -10,8 +11,10 @@ typedef struct _NODE
 
 Node* createNode(int key);
 int suffixsum(Node* head);
-void insertNode(Node** head, int keyvalue);
+void addNode(Node** head, int keyvalue);
 void displayList(Node* head);
+Node* searchNode(Node* head, int keyvalue);
+bool deleteNode(Node** head, int keyvalue);
 
 /*To run this code, use the following command: "./linkedlist.exe < "in.txt" > "out.txt""*/
 
@@ -81,7 +84,7 @@ int suffixsum(Node* head)
  * @param head Pointer to the pointer of the head node of the linked list.
  * @param keyvalue The key value of the new node to be inserted.
  */
-void insertNode(Node** head, int keyvalue) {
+void addNode(Node** head, int keyvalue) {
     Node* newNode = createNode(keyvalue);
     if(newNode == NULL) exit(1);
     newNode->next = (*head);
@@ -103,5 +106,54 @@ void displayList(Node* head) {
     }
     printf("NULL\n");  
 }
+
+/**
+ * @brief Searches for a node with the specified key value in a linked list.
+ *
+ * @param head The head node of the linked list.
+ * @param keyvalue The key value to search for.
+ * @return Returns a pointer to the node if found; otherwise, returns NULL.
+ */
+Node* searchNode(Node* head, int keyvalue){
+    Node* currentNode = head;
+
+    while(currentNode != NULL){
+        if(currentNode->key == keyvalue){
+            return currentNode;
+        }
+        currentNode = currentNode->next;
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief Deletes a node with the specified key value from a linked list.
+ *
+ * @param head A pointer to the head node of the linked list.
+ * @param keyvalue The key value of the node to be deleted.
+ * @return true if the node is found and deleted successfully; false if the node is not found.
+ */
+bool deleteNode(Node** head, int keyvalue){
+    Node* previousNode = NULL;
+    Node* currentNode = (*head);
+
+    while(currentNode != NULL){
+        if(currentNode->key == keyvalue){
+            if(previousNode == NULL){
+                (*head) = currentNode->next;
+            }
+            else{
+                previousNode->next = currentNode->next;
+            }
+            free(currentNode);
+            return true; // Node found and deleted successfully
+        }
+        previousNode = currentNode;
+        currentNode = currentNode->next;
+    }
+    return false; // Node not found
+}
+
 
 
