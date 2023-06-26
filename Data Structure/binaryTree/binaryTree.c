ad2689ecefef479cf11aeab8cc16433d2d8d6d94
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2023-06-26 00:37:36 -0300

    AddNode funtion

--- Data Structure/binaryTree/binaryTree.c
+++ Data Structure/binaryTree/binaryTree.c
@@ -1,23 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _NODE{
+typedef struct Node{
     int valueKey;
-    struct _Node* rightSubTree;
-    struct _Node* leftSubTree;
+    struct Node* rightSubTree;
+    struct Node* leftSubTree;
 }Node;
+
+void addNode(Node** root, int key);
+Node* createNode(int key);
 
 int main(void)
 {
     int keyAmount, treeHeight, i, key;
-    Node binaryTree;
+    Node* root = NULL;
 
-    scanf("%d %d", keyAmount, treeHeight);
+    scanf("%d %d", &keyAmount, &treeHeight);
 
     for(i = 0; i < keyAmount; i++){
-        scanf("%d", key);
-        addNode(key, binaryTree);
+        scanf("%d", &key);
+        addNode(&root, key);
     }
     
     return 0;
+}
+
+void addNode(Node** root, int key){
+    if((*root) == NULL){
+        (*root) = createNode(key);
+    }else{
+        if(key <= (*root)->valueKey){
+            addNode(&((*root)->leftSubTree), key);
+        }else{
+            addNode(&((*root)->rightSubTree), key);
+        }
+    }
+}
+
+Node* createNode(int key) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(newNode == NULL) exit(1);
+
+    newNode->valueKey = key;
+    newNode->leftSubTree = NULL;
+    newNode->rightSubTree = NULL;
+
+    return newNode;
 }
